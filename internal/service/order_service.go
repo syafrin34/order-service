58d5474b352351739b9a1f98a4b5549cf87bc130
syafrin ibrahim <syafrinibrahim12@gmail.com>
2025-08-23 08:52:49 +0800

    create go routine for cek stoock and pricing

--- internal/service/order_service.go
+++ internal/service/order_service.go
@@ -28,28 +28,105 @@ func NewOrderService(orderRepo repository.OrderRepository, productServiceURL, pr
 }
 
 func (o *OrderService)CreateOrder(order *entity.OrderEntity)(*entity.OrderEntity, error){
+	availabilityCh := make(chan struct{
+		ProductID int
+		Available bool
+		Err error
+	}, len(order.ProductRequests))
+
+	pricingCh := make(chan struct{
+		ProductID int
+		FinalPrice float64
+		MarkUp float64
+		Discount float64
+		Err error
+	}, len(order.ProductRequests))
+
+	
 	for _, productRequest := range order.ProductRequests{
+
+		// 1. proses syncronus
+
 		// check product availability
-		available, err := o.checkProductStock(productRequest.ProductID, productRequest.Quantity)
-		 if err != nil {
-			logger.Error().Err(err).Msgf("Error checking product stock for product %d", productRequest.ProductID)
-			return nil, err
-		 }
+		// available, err := o.checkProductStock(productRequest.ProductID, productRequest.Quantity)
+		//  if err != nil {
+		// 	logger.Error().Err(err).Msgf("Error checking product stock for product %d", productRequest.ProductID)
+		// 	return nil, err
+		//  }
 		// get pricing
-		pricing, err := o.GetPricing(productRequest.ProductID)
-		if err != nil {
-			logger.Error().Err(err).Msgf("Error getting pricing for product  %d", productRequest.ProductID)
-			return  nil, err
+		// pricing, err := o.GetPricing(productRequest.ProductID)
+		// if err != nil {
+		// 	logger.Error().Err(err).Msgf("Error getting pricing for product  %d", productRequest.ProductID)
+		// 	return  nil, err
+		// }
+
+		// if !available {
+		// 	logger.Warn().Msgf("product %d out of stock", productRequest.ProductID)
+		// 	return  nil, fmt.Errorf("product out of stock")
+		// }
+
+		// productRequest.FinalPrice = float64(productRequest.Quantity)*pricing.FinalPrice
+		// productRequest.MarkUp = float64(productRequest.Quantity)* pricing.Markup
+		// productRequest.Discount = float64(productRequest.Quantity)* pricing.Discount
+
+		
+		// 2. proses asyncronus
+		// go routine proses cek available stock product
+		go func(productRequest *entity.ProductRequest) {
+			available, err := o.checkProductStock(productRequest.ProductID, productRequest.Quantity)
+			availabilityCh <- struct{ProductID int; Available bool; Err error}{
+				ProductID: pricing.ProductID,
+				Available: available,
+				Err: err,
+			}
+		}(&productRequest)
+		
+		// go routine untuk pricing
+
+		go func(productRequest *entity.ProductRequest){
+			pricing, err := o.GetPricing(productRequest.ProductID)
+			pricingCh <-  struct{ProductID int; FinalPrice float64; MarkUp float64; Discount float64; Err error}{
+				ProductID: pricing.ProductID,
+				FinalPrice: pricing.FinalPrice,
+				MarkUp: pricing.Markup,
+				Discount: pricing.Discount,
+				Err: err,
+
+			}
+
+		}(&productRequest)
+
+
+	}
+
+	for range order.ProductRequests{
+		availabilityResult := <- availabilityCh
+		pricingResult  := <- pricingCh
+
+		if availabilityResult.Err != nil {
+			logger.Error().Err(availabilityResult.Err).Msgf("Error checking product stock for product %d", availabilityResult.ProductID)
+			return  nil, availabilityResult.Err
 		}
 
-		if !available {
-			logger.Warn().Msgf("product %d out of stock", productRequest.ProductID)
+		if !availabilityResult.Available {
+			logger.Warn().Msgf("product %d out of stock", availabilityResult.ProductID)
 			return  nil, fmt.Errorf("product out of stock")
 		}
 
-		productRequest.FinalPrice = float64(productRequest.Quantity)*pricing.FinalPrice
-		productRequest.MarkUp = float64(productRequest.Quantity)* pricing.Markup
-		productRequest.Discount = float64(productRequest.Quantity)* pricing.Discount
+		if pricingResult.Err != nil {
+			logger.Error().Err(pricingResult.Err).Msgf("Error getting pricing for product  %d", pricingResult.ProductID)
+			return  nil, pricingResult.Err
+		}
+
+		for _, productRequest := range order.ProductRequests{
+			if productRequest.ProductID == availabilityResult.ProductID{
+				productRequest.FinalPrice = float64(productRequest.Quantity)*pricingResult.FinalPrice
+				productRequest.MarkUp = float64(productRequest.Quantity)* pricingResult.MarkUp
+				productRequest.Discount = float64(productRequest.Quantity)* pricingResult.Discount 
+			}
+
+
+		}
 	}
 
 	// calculate order total 
